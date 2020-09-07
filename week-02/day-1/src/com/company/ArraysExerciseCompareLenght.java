0cd6dea1664bbd1d107963b3647eea71a2b5eaae
RobNikola911 <pajic1991@gmail.com>
2020-09-07 13:48:36 +0200

    ArraysExercise2

---
+++ week-02/day-1/src/com/company/ArraysExerciseCompareLenght.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class ArraysExerciseCompareLenght {
+    public static void main(String[] args) {
+        int[] firstArrayOfNumbers = {1, 2, 3};
+        int[] secondArrayOfNumbers = {4, 5};
+
+        if (secondArrayOfNumbers.length > firstArrayOfNumbers.length) {
+            System.out.println("secondArrayOfNumbers is longer");
+        }
+    }
+}
+// - Create an array variable named `firstArrayOfNumbers`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `secondArrayOfNumbers`
+//   with the following content: `[4, 5]`
+// - Print "secondArrayOfNumbers is longer" if `secondArrayOfNumbers` has more
+//   elements than `firstArrayOfNumbers`
