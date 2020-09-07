ca5a666abc3bfdcbbdda5f21b68ba2df3ffe2b35
RobNikola911 <pajic1991@gmail.com>
2020-09-07 16:24:43 +0200

    FunctionExerciseDoubling

---
+++ week-02/day-1/src/com/company/FunctionExercisesDoubling.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class FunctionExercisesDoubling {
+    public static void main(String[] args) {
+        int baseNum = 123;
+        System.out.println(doubling(baseNum));
+    }
+
+    public static int doubling(int n1) {
+        int doubling = n1 * 2;
+        return doubling;
+    }
+}
+
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
