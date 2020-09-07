d2776f16cec628d39cf2efc86a844a3b52ba4cd7
RobNikola911 <pajic1991@gmail.com>
2020-09-07 13:57:57 +0200

    ArraysExerciseChangeElements

---
+++ week-02/day-1/src/com/company/ArraysExerciseChangeElements.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ArraysExerciseChangeElements {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 8, 5, 6};
+        numbers[3] = 4;
+        System.out.println("The fourth element is: " + numbers[3]);
+    }
+}
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4
+// - Print the fourth element
