1a8189ceb39199c8b3a81bac321f7bba163358ee
RobNikola911 <pajic1991@gmail.com>
2020-09-07 13:53:31 +0200

    ArraysSumElements

---
+++ week-02/day-1/src/com/company/ArraysExerciseSumElements.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ArraysExerciseSumElements {
+    public static void main(String[] args) {
+        int[] numbers = {54, 23, 66, 12};
+        int sum = numbers[1] + numbers[2];
+        System.out.println("The sum of second and third element is: " + sum);
+    }
+}
+
+// - Create an array variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
