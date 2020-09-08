3375833c127ee430223327287a4541109c937df4
RobNikola911 <pajic1991@gmail.com>
2020-09-08 14:26:42 +0200

    StringMethodsToDoPrint

---
+++ week-02/day-2/src/com/company/StringMethodsToDoPrint.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class StringMethodsToDoPrint {
+    public static void main(String... args) {
+        String todoText = " - Buy milk\n";
+        // Add "My todo:" to the beginning of the todoText
+        todoText = "My todo:\n" + todoText + " - Download games\n" + " - Diablo".indent(4);
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected output:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+        System.out.println(todoText);
+    }
+}
