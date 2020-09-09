1da621a79a3e0498ef9ca33d840e83daede486de
RobNikola911 <pajic1991@gmail.com>
2020-09-09 17:55:31 +0200

    WriteMultipleLinesPractice

---
+++ week-02/day-3/src/com/company/WriteMultipleLinesPractice.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+public class WriteMultipleLinesPractice {
+    public static void main(String[] args) {
+        String myFilePath = "src/com/my-file.txt";
+        int fileLines = 5;
+        String world = "Banana";
+
+        getManipulate(myFilePath, world, fileLines);
+    }
+
+    private static void getManipulate(String path, String word, int lines) {
+        try {
+            FileWriter myFile = new FileWriter("my-file.txt");
+            for (int index = 0; index < lines; index++) {
+                Path filePath = Paths.get(path);
+                myFile.write(word + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
+}
+
+// Create a function that takes 3 parameters: a path, a word and a number
+// and is able to write into a file.
+//   The path parameter should be a string that describes the location of the file you wish to modify
+//   The word parameter should also be a string that will be written to the file as individual lines
+//   The number parameter should describe how many lines the file should have.
+//   If the word is 'apple' and the number is 5, it should write 5 lines into the file and each line should read 'apple'
+// The function should not raise any errors if it could not write the file.
