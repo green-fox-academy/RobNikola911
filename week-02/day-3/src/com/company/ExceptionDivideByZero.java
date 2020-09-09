9554c479646cac6401f4d83c9f3551c517ac64f6
RobNikola911 <pajic1991@gmail.com>
2020-09-09 13:09:48 +0200

    FunctionUpdateAdded

--- week-02/day-3/src/com/company/ExceptionDivideByZero.java
+++ week-02/day-3/src/com/company/ExceptionDivideByZero.java
@@ -7,14 +7,18 @@ public class ExceptionDivideByZero {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please add your number to divide 10: ");
         int divisor = scanner.nextInt();
+        System.out.println("The result is: " + func(divisor));
+    }
 
-
+    private static Integer func(int divisor) {
+        int result = 10;
         try {
-            int result = 10 / divisor;
-            System.out.println("The result is: " + result);
+            result = result / divisor;
+
         } catch (ArithmeticException e) {
             System.out.println("fail");
         }
+        return result;
     }
 }
 // Create a function that takes a number
