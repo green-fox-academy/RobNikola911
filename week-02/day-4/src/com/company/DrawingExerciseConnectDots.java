ceeb3adda797c25509879dcbdea7b379a63e9560
RobNikola911 <pajic1991@gmail.com>
2020-09-10 22:22:31 +0200

    DrawingExerciseConnectDots

---
+++ week-02/day-4/src/com/company/DrawingExerciseConnectDots.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class DrawingExerciseConnectDots {
+    public static void mainDraw(Graphics graphics) {
+        int[][] box = new int[][]{{10, 10}, {290, 10}, {290, 290}, {10, 290}};
+        int[][] dots = new int[][]{{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+                {120, 100}, {85, 130}, {50, 100}};
+        getArrayPoints();
+        // Create a function that takes 2 parameters:
+        // An array of {x, y} points and graphics
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+    }
+
+    private static void getArrayPoints() {
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
