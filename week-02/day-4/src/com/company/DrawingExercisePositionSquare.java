23614781d3dfe915658493185f29fddc6ac1130b
RobNikola911 <pajic1991@gmail.com>
2020-09-10 20:39:32 +0200

    DrawingExercisePositionSquare

---
+++ week-02/day-4/src/com/company/DrawingExercisePositionSquare.java
@@ -0,0 +1,55 @@
+package com.company;
+
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class DrawingExercisePositionSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        int x = 0;
+        int y = 0;
+
+        drawSquare(x, y, graphics);
+
+        // create a function that draws one square and takes 3 parameters:
+        // the x and y coordinates of the square's top left corner and the graphics
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+    }
+
+    private static void drawSquare(int x, int y, Graphics graphics) {
+        for (int i = 60; i <= 180; i += 60) {
+            graphics.setColor((new Color(i, i, i)));
+            graphics.fillRect(i + x, i + y, 50, 50);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
