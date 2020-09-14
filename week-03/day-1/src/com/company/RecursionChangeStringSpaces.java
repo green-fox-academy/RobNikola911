73e84fbb747d86afaa59b4413e3422c03706148e
RobNikola911 <pajic1991@gmail.com>
2020-09-14 15:36:46 +0200

    RecursionChangeStringSpaces

---
+++ week-03/day-1/src/com/company/RecursionChangeStringSpaces.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class RecursionChangeStringSpaces {
+    public static void main(String[] args) {
+        String sentence = "Why not to remove spaces?";
+        System.out.println(recursiveRemoveSpaces(sentence));
+    }
+
+    private static String recursiveRemoveSpaces(String spaceChange) {
+        if (spaceChange.length() == 0)
+            return spaceChange;
+        else if (spaceChange.charAt(0) == ' ') {
+            spaceChange = '*' + spaceChange.substring(1);
+        }
+        return spaceChange.charAt(0) + recursiveRemoveSpaces(spaceChange.substring(1));
+
+    }
+}
+//    Given a string, compute recursively a new string where all the adjacent chars are now separated by a *
