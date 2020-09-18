de515f42adc69110e54404930e0c3d711762e9cb
RobNikola911 <pajic1991@gmail.com>
2020-09-18 12:00:49 +0200

    SumTest

---
+++ week-03/day-5/Tests/ExerciseSumTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ExerciseSumTest {
+    private Sum arrayList;
+
+
+
+/*
+    Add a new test case
+    Instantiate your class
+    create a list of integers
+                    use the assertEquals to test the result of the created sum method*/
--- week-03/day-5/src/Sum.java
+++ week-03/day-5/src/Sum.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
+
 public class Sum {
+
+    public ArrayList<Integer> getArrayList(int integers) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int sumOfElements = 0;
+        for (int i = 0; i < getArrayList(integers).size(); i++) {
+           sumOfElements += getArrayList(integers).get(i);
+        }
+
+        return getArrayList(integers);
+    }
+
+
 }
+/*
+    Create a sum method in your class which has an ArrayList of Integers as parameter
+        It should return the sum of the elements in the list*/
