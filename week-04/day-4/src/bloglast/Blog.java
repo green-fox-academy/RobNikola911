ae0d0bd8e9541877231ddaf40b7c7136f47c2330
RobNikola911 <pajic1991@gmail.com>
2020-09-21 20:31:51 +0200

    relocated

--- week-04/day-4/src/bloglast/Blog.java
+++
@@ -1,9 +0,0 @@
-package bloglast;
-
-public class Blog {
-}
-/*
-    store a list of BlogPosts
-        add BlogPosts to the list
-        delete(int) one item at given index
-        update(int, BlogPost) one item at the given index and update it with another BlogPost*/
