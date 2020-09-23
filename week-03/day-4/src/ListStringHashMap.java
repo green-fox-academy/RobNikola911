e012dff85a969bb4d372498b71f33546a9853a45
RobNikola911 <pajic1991@gmail.com>
2020-09-23 17:49:07 +0200

    ListStringMapHelp

---
+++ week-03/day-4/src/ListStringHashMap.java
@@ -0,0 +1,46 @@
+public class ListStringHashMap {
+      /*
+  ArrayList --> Rugalmas és használhatóbb mint a tömbök.
+  List.add() -- Hozzá adja a listához.
+  List.addALL() -- Az adott listát adja hozzá a listához.
+  List.remove() -- Vagy az érték vagy az index alapján töröl a listából.
+  List.set() -- Átállítja az adott értéket.
+  List.size() -- A lista méretét adja meg
+  List.isEmpty() --  Boolean érték, hogy az adott lista üres-e
+  List.clear() -- Kitörli az egész listát.
+  List.contains() --  Keresés a tartalom között.
+  List.toArray() -- Az adott listát tömbbe rendezi.
+  List.toString() -- Stringé alakírja
+  String -- >
+  String.length() -- A string hosszát adja meg.
+  String.substring() -- levágja az adott stringet.
+  String.replace() -- Két String értéket cserél fel(egyiket a másik helyére rakja)
+  String.charAt() -- A megadott indexű charachtert adja vissza.
+  String.equals() -- Egyenlők-e
+  String.compareTo() -- Összehasolítás
+  String.toLowerCase() -- Kisbetű
+  String.toUpperCase() -- Nagyberű
+  String.contains() -- Tartalmaz-e
+  String.endsWith() -- Megvizsgálja, hogy azzal végződik-e
+  String.startsWith() -- Megvizsgálja, hogy azzal kezdődik-e
+  String.indexOf() -- Melyik indexen található.
+  String.lastIndexOF() -- Az utolsó index-e.
+  String.split() -- Szétválaszt.
+  HashMap --> Kulcs és Érték párok.
+  HashMap.put(kulcs,érték) -- Egy elem feltöltése az adott hashmap-be
+  HashMap.putAll(Map) -- Az adott Map feltöltése a mi Hashmap-ünkbe.
+  HashMap.get(kulcs) -- Az kulcshoz párosított értéket adja vissza.
+  HashMap.remove(kulcs) -- Eltávolít egy adott kulcsot és a hozzárendelt értéket, feltéve, hogy létezik.
+  HashMap.isEmpty() -- Ellenőrzi, hogy üres-e.
+  HashMap.size() -- Méretét adja vissza.
+  HashMap.keySet() -- Visszaadja a kulcsok halmazát amit foreach-el tudsz körbejárni.
+  HashMap.values() -- Visszaadja az értékek halmazát amit foreach-el tudsz körbejárni.
+  HashMap.containsKey(object) -- Igazat ad, ha az adott kulcs létezik
+  HashMap.containsValues(object) -- Igazat ad, ha az adott érték létezik valamelyik kulcs által.
+  HashMap.clear() -- Kiüríti.
+  HashMap.entrySet() -- Foreach-el egyzerre írja ki a kulcs-érték párokat.
+  Inheritance -- > Öröklődés
+  Interface -- > Interfészek */
+
+
+}
