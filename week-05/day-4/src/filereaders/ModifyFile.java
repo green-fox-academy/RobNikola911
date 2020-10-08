849a2d80703425d5f476bacb88fbb5abf999e2c9
RobNikola911 <pajic1991@gmail.com>
2020-10-09 01:13:50 +0200

    Modify File Content

--- week-05/day-4/src/filereaders/ModifyFile.java
+++ week-05/day-4/src/filereaders/ModifyFile.java
@@ -1,4 +1,34 @@
 package filereaders;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 public class ModifyFile {
+    public static void main(String[] args) {
+
+        writeMultipleLines("assets/rob.txt", "modify and verify", 4);
+    }
+
+    public static void writeMultipleLines(String str, String str2, int in) {
+        Path path = Paths.get(str);
+        try {
+            List<String> fromFile = Files.readAllLines(path);
+            for (int i = 0; i < in; i++) {
+                fromFile.add(str2 + " nr." + i);
+            }
+            Files.write(path, fromFile);
+
+            String str3 = Files.readString(path);
+            for (int i = 0; i < in; i++) {
+                str3 += str2 + "\n";
+            }
+            Files.write(path, Collections.singleton(str3));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
