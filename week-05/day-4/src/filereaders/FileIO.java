f5ad722ddfb519fbeb78018f0ae9745ed3a3a9fb
RobNikola911 <pajic1991@gmail.com>
2020-10-09 00:37:31 +0200

    FileIO copy and replace file content

---
+++ week-05/day-4/src/filereaders/FileIO.java
@@ -0,0 +1,32 @@
+package filereaders;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+public class FileIO {
+    public static void main(String[] args) {
+        Path sourceFile = Paths.get("assets/doubled.txt");
+        Path targetFile = Paths.get("assets/hello2.txt");
+        try {
+            Files.copy(sourceFile, targetFile,
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            System.err.format("I/O Error when copying file");
+        }
+        Path wiki_path = Paths.get("assets/hello2.txt");
+        Charset charset = Charset.forName("ISO-8859-1");
+        try {
+            List<String> lines = Files.readAllLines(wiki_path, charset);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
