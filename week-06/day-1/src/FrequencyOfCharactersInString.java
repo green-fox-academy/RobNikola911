e4accdb2792c38089e62756ba67da14c9fb8bc11
Robert Pajic <pajic1991@gmail.com>
2020-10-12 14:35:22 +0200

    Frequency of char in String(Map)

---
+++ week-06/day-1/src/FrequencyOfCharactersInString.java
@@ -0,0 +1,30 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.toMap;
+
+public class FrequencyOfCharactersInString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please give a word: ");
+        String word = sc.nextLine();
+
+        Map<Character, Integer> frequencies = word.chars().boxed()
+                .collect(toMap(
+                        k -> Character.valueOf((char) k.intValue()), // key = char
+                        counter -> 1,         // 1 occurence
+                        Integer::sum)); // counting
+        System.out.println("Frequencies:\n" + frequencies);
+
+/*        Map<Character, Integer> freqs = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            freqs.merge(c,                  // key = char
+                    1,                  // value to merge
+                    Integer::sum);      // counting
+        }
+        System.out.println("Frequencies:\n" + freqs);*/
+    }
+}
