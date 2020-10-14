c9806360b6f879b797734c6358826b2012228d83
Robert Pajic <pajic1991@gmail.com>
2020-10-14 18:41:50 +0200

    Fizz buzz font-size upgrade

--- week-06/day-3/src/main/java/com/greenfox/firstwebapp/controllers/FizzBuzzWoof.java
+++ week-06/day-3/src/main/java/com/greenfox/firstwebapp/controllers/FizzBuzzWoof.java
@@ -17,21 +17,28 @@ public class FizzBuzzWoof {
     public String fizzBuzzWoof(Model model) {
         long x = numCount.incrementAndGet();
         String fizzBuzzWoof = "";
+        int fontSize = 18;
 
         if (((x % 3) == 0) && ((x % 5) == 0) && ((x % 7) == 0)) {
             fizzBuzzWoof = "FizzBuzzWoof";
+            fontSize = 72;
         } else if (((x % 3) == 0) && ((x % 5) == 0)) {
             fizzBuzzWoof = "FizzBuzz";
+            fontSize = 48;
         } else if ((x % 3) == 0) {
             fizzBuzzWoof = "Fizz";
+            fontSize = 24;
         } else if ((x % 5) == 0) {
             fizzBuzzWoof = "Buzz";
+            fontSize = 24;
         } else if ((x % 7) == 0) {
             fizzBuzzWoof = "Woof";
+            fontSize = 24;
         } else {
             fizzBuzzWoof = String.valueOf(x);
         }
 
+        model.addAttribute("fontSize", fontSize);
         model.addAttribute("numcount", fizzBuzzWoof);
 
         return "fizzbuzzindex";
