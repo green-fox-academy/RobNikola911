5d63299ace838698b1ad5d08c03b2a25d4ea3b4e
Robert Pajic <pajic1991@gmail.com>
2020-10-16 08:10:57 +0200

    Bank account PostMapping

--- week-06/day-4/Practice/src/main/java/com/thymeleaf/exercise/controllers/BankAccountController.java
+++ week-06/day-4/Practice/src/main/java/com/thymeleaf/exercise/controllers/BankAccountController.java
@@ -5,6 +5,8 @@ import com.thymeleaf.exercise.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +19,10 @@ public class BankAccountController {
     public BankAccountController() {
         allAccounts.add(new BankAccount("Simba", 2000, "Lion"));
         allAccounts.add(new BankAccount("Zazu", 3000, "Bird"));
-        allAccounts.add(new BankAccount("Pumbaa", 1500, "Pig"));
+        allAccounts.add(new BankAccount("Pumba", 1500, "Pig"));
         allAccounts.add(new BankAccount("Ed", 500, "Hyena"));
         allAccounts.add(new BankAccount("Musafa", 5000, "King"));
-        allAccounts.add(new BankAccount("Lana", 3250, "Giraffe"));
+        allAccounts.add(new BankAccount("Lana", 2500, "Giraffe"));
     }
 
     @GetMapping("/show")
@@ -39,6 +41,25 @@ public class BankAccountController {
 
     @GetMapping("/showAll")
     public String showAllAccounts(Model model) {
+        model.addAttribute("accounts", allAccounts);
+        return "bank-accounts";
+    }
+
+    @PostMapping("/showAll")
+    public String raiseBalanceAndShowAllAccounts(Model model, @RequestParam(required = false) String name) {
+        Integer increase = 10;
+        if( allAccounts.stream()
+                .filter(a -> a.getName().equals(name))
+                .findAny()
+                .get().getAnimalType() == "King")
+            increase = 100;
+        allAccounts.stream()
+                .filter(a -> a.getName().equals(name))
+                .findAny()
+                .get().setBalance(
+                allAccounts.stream()
+                        .filter(a -> a.getName().equals(name))
+                        .findAny().get().getBalance()+increase);
         model.addAttribute("accounts", allAccounts);
         return "bank-accounts";
     }
