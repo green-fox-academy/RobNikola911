34b33b167b7fd402fe02b10a80f55fd50ee9a4d7
Robert Pajic <pajic1991@gmail.com>
2020-10-16 09:49:35 +0200

    Shop item model

---
+++ week-06/day-5/WebShopApp/src/main/java/com/robwebshop/onlinewebshop/OnlinewebshopApplication.java
@@ -0,0 +1,13 @@
+package com.robwebshop.onlinewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinewebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinewebshopApplication.class, args);
+    }
+
+}
---
+++ week-06/day-5/WebShopApp/src/test/java/com/robwebshop/onlinewebshop/OnlinewebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robwebshop.onlinewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinewebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
