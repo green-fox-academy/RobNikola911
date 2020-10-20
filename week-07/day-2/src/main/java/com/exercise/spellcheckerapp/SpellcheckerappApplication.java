a260861c6e0288f62f492092c3622b54e0d8b933
Robert Pajic <pajic1991@gmail.com>
2020-10-20 11:52:34 +0200

    Spellchecker exercise

---
+++ week-07/day-2/src/main/java/com/exercise/spellcheckerapp/SpellcheckerappApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.spellcheckerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellcheckerappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpellcheckerappApplication.class, args);
+    }
+
+}
