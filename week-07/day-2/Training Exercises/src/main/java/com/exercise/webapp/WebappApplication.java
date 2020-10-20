22210b78c458e9f398069e5c624ab4896c3f5e7f
Robert Pajic <pajic1991@gmail.com>
2020-10-20 13:28:11 +0200

    HelloBeanWorld dependency injection

--- week-07/day-2/Training Exercises/src/main/java/com/exercise/webapp/WebappApplication.java
+++ week-07/day-2/Training Exercises/src/main/java/com/exercise/webapp/WebappApplication.java
@@ -1,13 +1,27 @@
 package com.exercise.webapp;
 
+import com.exercise.webapp.services.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication implements CommandLineRunner {
+
+    private final Printer printer;
+
+    @Autowired
+    public WebappApplication(Printer printer) {
+        this.printer = printer;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        printer.log("Hello Bean!");
+    }
 }
