7fa51e5cc77e246118484d3ceafb2994ac581739
Robert Pajic <pajic1991@gmail.com>
2020-10-20 17:17:55 +0200

    encoder and decoder

---
+++ week-07/day-2/Training Exercises/src/main/java/com/exercise/webapp/controllers/UtilityController.java
@@ -0,0 +1,62 @@
+package com.exercise.webapp.controllers;
+
+import com.exercise.webapp.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UtilityController {
+
+    private final UtilityService utilityService;
+
+    @Autowired
+    public UtilityController(UtilityService utilityService) {
+        this.utilityService = utilityService;
+    }
+
+    @GetMapping("/useful")
+    public String usefulLinks(Model model){
+        model.addAttribute("random", utilityService.randomColor());
+        model.addAttribute("caesar", utilityService.caesar("abc", 10));
+        return "index";
+    }
+
+    @GetMapping("/useful/colored")
+    public String usefulColored(Model model){
+        model.addAttribute("random", utilityService.randomColor());
+        return "colored";
+    }
+
+    @GetMapping("/useful/email")
+    public String usefulEmail(Model model, @RequestParam(value = "email", required = false) String email){
+        model.addAttribute("email", utilityService.validateEmail(email));
+        return "email";
+    }
+
+    @GetMapping("/useful/encoder")
+    public String ceasarEncoder(Model model, @RequestParam(value = "encoder", required = false) String text,
+                                            @RequestParam int number){
+        model.addAttribute("encoder", utilityService.caesar("abc", 1));
+        return "encoder";
+    }
+
+    @GetMapping("/useful/decoder")
+    public String ceasarDecoder(Model model, @RequestParam(value = "decoder", required = false) String text,
+                                             @RequestParam int number){
+        model.addAttribute("decoder", utilityService.caesar("abc", 1));
+        return "decoder";
+    }
+
+
+// ## Caesar encoder/decoder
+// - Caesar coding is just shifting every character in the text with a specific number of amount
+// in the alphabet, coded `example` with `1` is `fybnqmf`._
+// - Have two endpoints (one for encoding and one for decoding) with a `text` and `number` query param
+// - Have a link at the main page for both
+// - For encoding use the `number` value, for decoding use the `-number` value when calling the
+// caesar() method.
+//     - As a rule of usage: you cannot give negative value for the 'number' query param
+}
---
+++ week-07/day-2/Training Exercises/src/main/java/com/exercise/webapp/services/UtilityService.java
@@ -0,0 +1,53 @@
+package com.exercise.webapp.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class UtilityService {
+
+    private List<String> colors;
+    private Random random;
+
+    public UtilityService() {
+        colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("blue");
+        colors.add("lime");
+        colors.add("orange");
+        colors.add("magenta");
+        random = new Random();
+    }
+
+    public String randomColor() {
+        return colors.get(random.nextInt(colors.size()));
+    }
+
+
+    public String caesar(String text, int number) {
+        if (number < 0) {
+            number = 26 + number;
+        }
+
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            int offset = Character.isUpperCase(text.charAt(i)) ? 'A' : 'a';
+            result += (char) (((int) text.charAt(i) + number - offset) % 26 + offset);
+        }
+        return result;
+    }
+
+    public boolean validateEmail(String email) {
+        if (email == null){
+            return false;
+        }
+
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+}
