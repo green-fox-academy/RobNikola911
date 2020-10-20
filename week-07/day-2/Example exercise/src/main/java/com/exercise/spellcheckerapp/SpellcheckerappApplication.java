c63bc3b1f7f1319173431c65120a6f572c397a16
Robert Pajic <pajic1991@gmail.com>
2020-10-20 13:11:07 +0200

    Example spellchecker#

--- week-07/day-2/Example exercise/src/main/java/com/exercise/spellcheckerapp/SpellcheckerappApplication.java
+++ week-07/day-2/Example exercise/src/main/java/com/exercise/spellcheckerapp/SpellcheckerappApplication.java
@@ -1,13 +1,26 @@
 package com.exercise.spellcheckerapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpellcheckerappApplication {
+public class SpellcheckerappApplication implements CommandLineRunner {
+
+    private SpellChecker checker;
+
+    @Autowired
+    SpellcheckerappApplication(SpellChecker checker) {
+        this.checker = checker;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpellcheckerappApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        checker.checkSpelling();
+    }
 }
