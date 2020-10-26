eca32b8007bc5ec213799ca9821d6c6250fc95d2
Robert Pajic <pajic1991@gmail.com>
2020-10-26 11:46:01 +0100

    Todo H2 model done

---
+++ week-08/day-1/Listing Todos H2/src/main/java/com/listingtodos/embeddeddb/EmbeddedDbApplication.java
@@ -0,0 +1,13 @@
+package com.listingtodos.embeddeddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedDbApplication.class, args);
+    }
+
+}
---
+++ week-08/day-1/Listing Todos H2/src/main/java/com/listingtodos/embeddeddb/models/Todo.java
@@ -0,0 +1,50 @@
+package com.listingtodos.embeddeddb.models;
+
+public class Todo {
+
+    private Long id;
+    private String title;
+    private Boolean urgent;
+    private Boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(String title, Boolean urgent, Boolean done) {
+        this.title = title;
+        this.urgent = urgent;
+        this.done = done;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getUrgent() {
+        return urgent;
+    }
+
+    public void setUrgent(Boolean urgent) {
+        this.urgent = urgent;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
