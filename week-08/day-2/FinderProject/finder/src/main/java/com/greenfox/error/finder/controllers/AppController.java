7f79e232526416d2bce5ca9a597e2ef9382275ba
Robert Pajic <pajic1991@gmail.com>
2020-10-27 14:33:09 +0100

    error finder done

--- week-08/day-2/FinderProject/finder/src/main/java/com/greenfox/error/finder/controllers/AppController.java
+++ week-08/day-2/FinderProject/finder/src/main/java/com/greenfox/error/finder/controllers/AppController.java
@@ -1,11 +1,12 @@
-package controller;
+package com.greenfox.error.finder.controllers;
 
-import service.UserService;
-import com.greenfox.error.model.User;
+import com.greenfox.error.finder.services.UserService;
+import com.greenfox.error.finder.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,19 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/app")
 public class AppController {
 
-    @Autowired
-    UserService service;
+    private final UserService service;
 
-    @GetMapping("/")
+    @Autowired
+    public AppController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping({"/", ""})
     public String index(Model model) {
         model.addAttribute("new_user", new User());
         model.addAttribute("yolo", service.getAll());
         return "index";
     }
 
-    @PostMapping("/app")
-    public String create() {
-        service.save(new User());
-        return "redirect:/";
+    @PostMapping("")
+    public String create(@ModelAttribute User user) {
+        service.save(user);
+        return "redirect:/app";
     }
 }
--- week-08/day-2/FinderProject/finder/src/main/java/com/greenfox/error/finder/services/UserService.java
+++ week-08/day-2/FinderProject/finder/src/main/java/com/greenfox/error/finder/services/UserService.java
@@ -1,15 +1,17 @@
-package service;
+package com.greenfox.error.finder.services;
 
-import com.greenfox.error.model.User;
+import com.greenfox.error.finder.models.User;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 /**
  * Created by aze on 25/10/17.
  */
+@Service
 public class UserService {
 
-    private ArrayList<User> users;
+    private final ArrayList<User> users;
 
     public UserService() {
         this.users = new ArrayList<>();
