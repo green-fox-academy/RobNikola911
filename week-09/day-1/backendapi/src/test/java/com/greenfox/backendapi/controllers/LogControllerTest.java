74d7248015d1603e44608c9a52b53067e3c6d9c6
Robert Pajic <pajic1991@gmail.com>
2020-11-06 12:02:22 +0100

    Log test done

--- week-09/day-1/backendapi/src/test/java/com/greenfox/backendapi/controllers/LogControllerTest.java
+++ week-09/day-1/backendapi/src/test/java/com/greenfox/backendapi/controllers/LogControllerTest.java
@@ -1,5 +1,7 @@
 package com.greenfox.backendapi.controllers;
 
+import com.greenfox.backendapi.DTOs.LogDTO;
+import com.greenfox.backendapi.models.Log;
 import com.greenfox.backendapi.services.LogService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -22,21 +24,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(LogController.class)
 public class LogControllerTest {
 
+
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private LogService logService;
 
+
+    private LogDTO getLogsDTO(){
+        LogDTO testLogs = new LogDTO();
+        testLogs.setEntries(Arrays.asList(new Log("\"/arrays\"", "\"double,[1, 2, 5, 10]\""),
+                new Log("\"/arraytest\"", "\"double,[1, 2, 5, 11]\"")));
+        testLogs.setEntry_count(2);
+        return testLogs;
+    };
+
     @Test
     public void logServiceTest() throws Exception {
 
-        when(logService.logEntries()).thenReturn(Arrays.asList());
+        when(logService.logEntries()).thenReturn(getLogsDTO());
 
         mockMvc.perform(get("/log"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(24)))
-                .andExpect(jsonPath("$[0].id",is(1)));
+                .andExpect(jsonPath("$.entries",hasSize(2)))
+                .andExpect(jsonPath("$.entries[1].endpoint",is("\"/arraytest\"")));
+    }
+
+    @Test
+    public void logServiceEntrycount() throws Exception {
+        when(logService.logEntries()).thenReturn(getLogsDTO());
+
+        mockMvc.perform(get("/log"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.entry_count",is(2)));
     }
 
 }
---
+++ week-09/day-5/src/main/java/com/greenfox/resttest/controllers/GuardianController.java
@@ -0,0 +1,2 @@
+package com.greenfox.resttest.controllers;public class GuardianController {
+}
